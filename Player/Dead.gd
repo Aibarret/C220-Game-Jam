b2385556965933a504341990e4cce43b1d76f6de
airtoumk <aidanlesh@gmail.com>
2021-04-21 19:10:07 -0400

    Created Player
    jesus christ man your placeholder files created a bunch of merge conflicts

---
+++ Player/Dead.gd
@@ -0,0 +1,11 @@
+extends "res://StateMachine/StateMachineState.gd"
+
+
+
+
+
+func start():
+	myEnt.velocity = Vector2.ZERO
+
+func physics_process(_delta):
+	pass
--- Player/Player.gd
+++ Player/Player.gd
@@ -1,72 +1,109 @@
 extends KinematicBody2D
 
+onready var Global = get_node("/root/Global")
+
 onready var SM = $StateMachine
+onready var tilemap = get_node_or_null("/root/Game/TileMap")
 
-var velocity = Vector2.ZERO
-var jump_power = Vector2.ZERO
-var direction = 1
+var velocity = Vector2(0.0, 0.0)
+var normal_gravity = 1550.0
+var jump_gravity = 900.0
+var acceleration = Vector2(0.0, normal_gravity)
+var move_speed = 300
+var jump_speed = -560
+var run_and_jump_speed = -640
+var jump_released = true
+var flipH = false
+var default_coyote_time = 4.0 / 60.0 # seconds
+var coyote_time = 0.0
+var dead_timer = 0.0
+var respawn_time = 2.0
 
-export var gravity = Vector2(0,30)
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	add_to_group("Player")
 
-export var move_speed = 20
-export var max_move = 300
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$State.text = SM.state_name
+	$AnimatedSprite.flip_h = flipH
+	#$AnimatedSprite.offset.x = 1 if flipH else -1
+	#if velocity.y > 0 and $AnimatedSprite.animation == "Jumping":
+	#	$AnimatedSprite.play("Falling_Start")
 
-export var jump_speed = 100
-export var max_jump = 1000
-
-export var leap_speed = 100
-export var max_leap = 1000
-
-var moving = false
-var is_jumping = false
-var double_jumped = false
-
-
-
-func _physics_process(_delta):
-	velocity.x = clamp(velocity.x,-max_move,max_move)
-		
-	if direction < 0 and not $AnimatedSprite.flip_h: $AnimatedSprite.flip_h = true
-	if direction > 0 and $AnimatedSprite.flip_h: $AnimatedSprite.flip_h = false
+func _physics_process(delta):
+	if SM.state_name == "Dead":
+		#$CollisionPolygon2D.disabled = true
+		dead_timer += delta
+		if dead_timer > respawn_time:
+			pass
+		return
+	if Input.is_action_just_released("up"):
+		jump_released = true
+	velocity += acceleration * delta
+	velocity = move_and_slide(velocity, Vector2.UP, false)
+	handle_direction(Input.is_action_pressed("left"), Input.is_action_pressed("right"))
+	if is_on_wall():
+		velocity.x = 0
+	coyote_time -= delta # delta because it's in seconds
+	"""
+	for i in get_slide_count():
+		var collision = get_slide_collision(i)
+		if collision.collider == tilemap:
+			# I test each collision at 4 different points just in case if it's on a bad edge or corner
+			for x in [-1, 1]:
+				for y in [-1, 1]:
+					var tile = get_tile_at_pos(collision.position + Vector2(x * 0.1, y * 0.1))
+					if tile == 14 or tile == 15: # steven tile
+						die()
+	var tile = get_tile_at_pos(position)
+	match tile:
+		2: # spikes
+			die()
+		4: # uppinators
+			velocity.y += -Global.antigrav_tile_strength * delta
+			if SM.state_name != "Falling":
+				SM.set_state("Falling")
+			set_animation("Falling")
+		6: # oil
+			set_tile_at_pos(position, TileMap.INVALID_CELL)
+			Global.score += 1
+		13: # oil on bg
+			set_tile_at_pos(position, 12) # set to bg
+			Global.score += 1
+		16: # door, top
+			Global.load_next_level()
+			return
+		17: # door, bottom
+			Global.load_next_level()
+			return
+	"""
 	
-	if is_on_floor():
-		double_jumped = false
-
-func is_moving():
-	if Input.is_action_pressed("left") or Input.is_action_pressed("right"):
-		return true
-	return false
-
-func move_vector():
-	return Vector2(Input.get_action_strength("right") - Input.get_action_strength("left"),1.0)
-
-func _unhandled_input(event):
-	if event.is_action_pressed("left"):
-		direction = -1
-	if event.is_action_pressed("right"):
-		direction = 1
-
-func set_animation(anim):
-	if $AnimatedSprite.animation == anim: return
-	if $AnimatedSprite.frames.has_animation(anim): $AnimatedSprite.play(anim)
-	else: $AnimatedSprite.play()
-
-func is_on_floor():
-	var fl = $Floor.get_children()
-	for f in fl:
-		if f.is_colliding():
-			return true
-	return false
-
-func is_on_right_wall():
-	if $Wall/Right.is_colliding():
-		return true
-	return false
-
-func is_on_left_wall():
-	if $Wall/Right.is_colliding():
-		return true
-	return false
+func get_tile_at_pos(pos):
+	var input_position = pos - tilemap.position
+	input_position /= tilemap.scale.x
+	var grid_position = tilemap.world_to_map(input_position)
+	var tile = tilemap.get_cellv(grid_position)
+	return tile
+	
+func set_tile_at_pos(pos, tile):
+	var input_position = pos - tilemap.position
+	input_position /= tilemap.scale.x
+	var grid_position = tilemap.world_to_map(input_position)
+	tilemap.set_cellv(grid_position, tile)
 
 func die():
-	queue_free()
+	SM.set_state("Dead")
+	
+func set_animation(anim):
+	if $AnimatedSprite.animation != anim:
+		$AnimatedSprite.play(anim)
+
+
+func _on_AnimatedSprite_animation_finished():
+	pass
+
+func handle_direction(isleft, isright):
+	if isleft != isright:
+		flipH = isleft
+
