19b13ec2d8412ff9ff75d0315088ddf6a972c23b
apmolina <apmolina@iu.edu>
2021-04-22 13:53:17 -0400

    Menus and Timer addition

--- Game/global.gd
+++ Game/global.gd
@@ -1,11 +1,37 @@
 extends Node
 
+var time = 0
+var level = 1
+
+signal time_changed
 
 func _ready():
-	pass 
+	pause_mode = Node.PAUSE_MODE_PROCESS
+	
+	if level == 1:
+		time = 10
+	
+
+func _unhandled_input(event):
+	if event.is_action_pressed("quit"): 
+		var menu = get_node_or_null("/root/Game/UI/Menu")
+		if menu != null:
+			var p = get_tree().paused
+			if p:
+				menu.hide()
+				get_tree().paused = false
+			else:
+				menu.show()
+				get_tree().paused = true
 
 
+func change_time():
+	if level == 1:
+		time -= 1
+		if time <= 0:
+			time = 0
+			get_tree().change_scene("res://UI/Main Menu.tscn")
+	else:
+		time += 1
+	emit_signal("time_changed")
 
-func _process(delta):
-	if Input.is_action_pressed("quit"): 
-		get_tree().quit()
--- UI/HUD.gd
+++ UI/HUD.gd
@@ -1,10 +1,11 @@
 extends Control
 
-
-
 func _ready():
-	update_score()
+	var _time_changed = Global.connect("time_changed",self,"_on_time_changed")
+	_on_time_changed()
 
-func update_score():
-	$Score.text = "Score: " + str(Global.save_data["score"])
-	
+func _on_time_changed():
+	$Time.text = "Time: " + str(Global.time)
+
+func _on_Timer_timeout():
+	Global.change_time()
