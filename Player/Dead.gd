ffc663701fc4e41abacb8223d475f664fa2de09c
airtoumk <aidanlesh@gmail.com>
2021-04-23 14:12:15 -0400

    Added Tombstone
    I ought to appear whenever a player dies

--- Player/Dead.gd
+++ Player/Dead.gd
@@ -1,11 +1,13 @@
 extends "res://StateMachine/StateMachineState.gd"
 
-
+export var Tombstone : PackedScene
 
 
 
 func start():
 	myEnt.velocity = Vector2.ZERO
+	var tombstone = Tombstone.instance()
+	tombstone.global_position = myEnt.global_position
 
 func physics_process(_delta):
 	pass
