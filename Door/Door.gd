a39b064c6c01ea82250f3f9722db07ddab2cd3ba
airtoumk <aidanlesh@gmail.com>
2021-04-24 16:44:51 -0400

    Door Texture Slot

--- Door/Door.gd
+++ Door/Door.gd
@@ -2,10 +2,13 @@ extends Area2D
 
 onready var Global = get_node("/root/Global")
 
+export var door_texture : Texture
+
 var open = true
 
 func _ready():
-	pass 
+	if door_texture != null:
+		$Sprite.texture = door_texture 
 
 func _on_Door_body_entered(body):
 	if body.name == "Player" and open:
